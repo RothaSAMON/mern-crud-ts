157836a53b054b73c981b917a0fb7b2d038f8f30
Rotha SAMON <samonrotha@gmail.com>
2024-11-26 13:30:32 +0700

    Modify tsconfig for TypeORM & Create Get All User and Post User

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,37 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import cors from "cors";
+import { schema } from "./Schema";
+import { createConnection } from "typeorm";
+import { Users } from "./Entities/Users";
+
+const main = async () => {
+  await createConnection({
+    type: "mysql",
+    database: "GraphqlCRUD",
+    username: "root",
+    password: "smeryoung097",
+    logging: true,
+    synchronize: false,
+    entities: [Users],
+  });
+
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+    })
+  );
+
+  app.listen(3001, () => {
+    console.log("Server running on port 3001 🚀");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
