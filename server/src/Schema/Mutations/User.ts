a1ead33b2f919b9715b59dd946c7e3ef3561eb57
Rotha SAMON <samonrotha@gmail.com>
2024-11-26 14:27:52 +0700

    Added Update password and return message

--- server/src/Schema/Mutations/User.ts
+++ server/src/Schema/Mutations/User.ts
@@ -1,4 +1,5 @@
-import { GraphQLString } from "graphql";
+import { GraphQLID, GraphQLString } from "graphql";
+import { MessageType } from "../TypeDefs/Messages";
 import { UserType } from "../TypeDefs/User";
 import { Users } from "../../Entities/Users";
 
@@ -19,5 +20,47 @@ export const CREATE_USER = {
       password,
     });
     return args;
+  },
+};
+
+export const DELETE_USER = {
+  type: MessageType,
+  args: {
+    id: { type: GraphQLID },
+  },
+
+  async resolve(parent: any, args: any) {
+    const { id } = args;
+    await Users.delete(id);
+
+    return { successful: true, message: "Delete Successful!" };
+  },
+};
+
+export const UPDATE_PASSWORD = {
+  type: MessageType,
+  args: {
+    username: { type: GraphQLString },
+    oldPassword: { type: GraphQLString },
+    newPassword: { type: GraphQLString },
+  },
+
+  async resolve(parent: any, args: any) {
+    const { username, oldPassword, newPassword } = args;
+    const user = await Users.findOne({ where: { username: username } });
+
+    if (!user) {
+      throw new Error("Username doesn't exit!");
+    }
+
+    const userPassword = user?.password;
+
+    if (oldPassword === userPassword) {
+      await Users.update({ username: username }, { password: newPassword });
+
+      return { successful: true, message: "Password update Successful!" };
+    } else {
+      throw new Error("Passwords do not match!");
+    }
   },
 };
